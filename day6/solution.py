3bf7ec8b76e7ab7023b743b3615864d3ed9dcd78
Bartosz <bartoszpietrzak.dev@gmail.com>
2022-12-06 21:43:49 +0100

    part 2 solution

--- day6/solution.py
+++ day6/solution.py
@@ -1,16 +1,13 @@
-message = open('test.txt','r').read()
+message = open('/home/bartosz/projects/aoc2022/day6/message.txt','r').read()
 cBuff = []
-toProcessBeforeMsg, uniqueCs = 0, 4
-for i in range(0,2):
-    print(uniqueCs)
+toProcessBeforeMsg = 0
+uniqueCs = [4, 14]
+for uniqC in uniqueCs:
     for index, c in enumerate(message):
-
-        if index > (uniqueCs-1):
-            cBuff = message[index-(uniqueCs-1):index]
-            if(uniqueCs == 14):
-                print(True if len(cBuff) == len(set(cBuff)) else False)
+        if index > (uniqC-1):
+            cBuff = message[index-(uniqC-1):index]
             if c not in cBuff and len(cBuff) == len(set(cBuff)):
                 if not toProcessBeforeMsg:
                     toProcessBeforeMsg = index + 1
                     print(cBuff+c, toProcessBeforeMsg)
-    uniqueCs = 14
+    toProcessBeforeMsg=0
